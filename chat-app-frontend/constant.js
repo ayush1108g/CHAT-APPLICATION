f5ff558c3637672990f9d7bf5c9fc07c26f9bdf3
ayush1108g <105841420+ayush1108g@users.noreply.github.com>
2024-04-17 20:58:27 +0530

    fixed some errors

--- chat-app-frontend/constant.js
+++ chat-app-frontend/constant.js
@@ -1,4 +1,7 @@
 // export const baseBackendUrl = "http://127.0.0.1:8000/api/v1";
 
+// export const baseBackendUrl =
+//   "https://cp29bd07-8000.inc1.devtunnels.ms/" + "api/v1";
+
 export const baseBackendUrl =
-  "https://cp29bd07-8000.inc1.devtunnels.ms/" + "api/v1";
+  "https://chat-application-l0z7.onrender.com/api/v1";
